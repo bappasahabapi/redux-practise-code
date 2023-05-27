d9d60e6d57536ce311333b8d6e56374720c06c09
Your GitHub Username <Your GitHub Email Address>
2023-05-27 23:12:23 +0600

    simple-todo-3 v1

---
+++ simple-todo-3/src/redux/todos/initialState.js
@@ -0,0 +1,15 @@
+const initialState = [
+    {
+        id: 1,
+        text: "There is a dreamy boy whose name is bappa",
+        completed: true,
+    },
+    {
+        id: 2,
+        text: "Cogito Egro Sum ",
+        completed: false,
+        color: "red",
+    },
+];
+
+export default initialState;
