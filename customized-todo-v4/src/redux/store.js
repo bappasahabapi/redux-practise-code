4a7c67a9256a83a88a800f2f6019d47f2d4448e6
Your GitHub Username <Your GitHub Email Address>
2023-05-27 23:30:50 +0600

    v5

--- customized-todo-v4/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./rootReducer";
-import thunkMiddleware from "redux-thunk";
-
-const store = createStore(rootReducer, composeWithDevTools
-    (applyMiddleware(thunkMiddleware)));
-
-export default store;
